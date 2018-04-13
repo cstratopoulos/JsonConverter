97e08927b2edc99adbb28306a8012092863a4870
Christos Stratopoulos <christos.stratopoulos@ossiaco.com>
2018-04-13 16:33:07 -0400

    Add type_tree

---
+++ include/ossiaco/converter/core/type_tree.hpp
@@ -0,0 +1,99 @@
+#ifndef OSSIACO_CONVERTER_TYPE_TREE_HPP
+#define OSSIACO_CONVERTER_TYPE_TREE_HPP
+
+#include <ossiaco/converter/utils/is_detected.hpp>
+
+#include <boost/mp11/algorithm.hpp>
+#include <boost/mp11/function.hpp>
+#include <boost/mp11/integral.hpp>
+#include <boost/mp11/list.hpp>
+#include <boost/mp11/map.hpp>
+
+#include <type_traits>
+
+namespace Ossiaco::converter {
+
+/// Customization point for enums that provide enum-mapped JSON conversion.
+///
+/// A `TypeTreeNode` for `Enum` is a structure that exposes a public `Map` type alias.
+/// \requires `Enum` is an enum.
+/// \see [providesTypeTreeBasic], [isTypeTreeLeaf], [isTypeTreeNonLeaf] for concept checking.
+template<typename Enum>
+struct TypeTreeNode : std::false_type {
+    /// Suggested helper alias for defining map entries.
+    template<Enum e, typename T>
+    using MapEntry = boost::mp11::mp_list<std::integral_constant<Enum, e>, T>;
+
+    /// The type alias that must be exposed for generation of enum-to-type maps.
+    ///
+    /// A valid `Map` is a nonempty list of `MapEntry<e, T>` where `T` is either
+    /// - a class/struct; or
+    /// -an enumeration SubEnum such that `TypeTreeNode<SubEnum>` is a valid `TypeTreeNode`.
+    using Map = void;
+
+    /// The name of the JSON member field from which type info should be extracted.
+    static constexpr string_view_t typeFieldName() = delete;
+
+    /// The default value that should be used for a JSON entry which is not in `Map`.
+    ///
+    /// `Map` must contain `MapEntry<defaultVal(), T>` for some `T`.
+    static constexpr Enum defaultVal() = delete;
+};
+
+/// The codomain of `TypeTreeNode<Enum>::Map`: all possible mapped classes.
+template<typename Enum>
+using TreeMappedClasses = boost::mp11::mp_unique<
+    boost::mp11::mp_transform<
+    boost::mp11::mp_second, typename TypeTreeNode<Enum>::Map
+    >
+>;
+
+namespace detail {
+
+//=== Helper traits for concept checking below ===//
+
+template<typename Enum>
+using TreeNodeMap = typename TypeTreeNode<Enum>::Map;
+
+template<typename Enum>
+using TreeNodeTypeField = decltype(TypeTreeNode<Enum>::typeFieldName());
+
+template<typename Enum>
+using TreeNodeDefaultVal = decltype(TypeTreeNode<Enum>::defaultVal());
+
+template<typename MapEntry>
+using IsPair = std::is_same<
+    boost::mp11::mp_size<MapEntry>, boost::mp11::mp_size_t<2>>;
+
+template <typename MapType>
+using IsValidTreeMap = boost::mp11::mp_and<
+    boost::mp11::mp_not<boost::mp11::mp_empty<MapType>>, // is nonempty
+    boost::mp11::mp_is_map<MapType>,                     // is a map
+    boost::mp11::mp_all_of<MapType, IsPair>              // all entries are pairs
+>;
+
+// Checks if TypeTreeNode<Enum> provides the basis for a valid TypeTreeNode specialization.
+template<typename Enum>
+using ProvidesTypeTreeBasic = boost::mp11::mp_and<
+    boost::mp11::mp_not<std::is_base_of<std::false_type, TypeTreeNode<Enum>>>,
+    std::is_enum<Enum>,
+    detail::IsValidTreeMap<stdxp::detected_t<TreeNodeMap, Enum>>,
+    std::is_same<string_view_t, stdxp::detected_t<TreeNodeTypeField, Enum>>,
+    std::is_same<Enum, stdxp::detected_t<TreeNodeDefaultVal, Enum>>
+>;
+
+// Checks if a TypeTreeNode MapType is a leaf: none of its mapped values are enums.
+// This check should not be used in isolation as it will return true on an empty list.
+template <typename Enum>
+using IsTreeMapLeaf = boost::mp11::mp_none_of<
+    boost::mp11::mp_transform<
+        boost::mp11::mp_second,
+        stdxp::detected_t<TreeNodeMap, Enum>>,
+    std::is_enum
+>;
+
+} // namespace detail
+
+} // namespace Ossiaco::converter
+
+#endif // OSSIACO_CONVERTER_TYPE_TREE_HPP
