3607bf3a490557b7691359f026c7ef6abb109f3d
Christos Stratopoulos <christos.stratopoulos@ossiaco.com>
2018-04-13 16:53:35 -0400

    Add license_brief and put it in front of all files

--- include/ossiaco/converter/utils/print_type_name.hpp
+++ include/ossiaco/converter/utils/print_type_name.hpp
@@ -1,3 +1,13 @@
+// Ossiaco JSON Converter Library
+// 
+// Copyright (C) 2018 Ossiaco
+// 
+// Licensed under the MIT license ("the license"); you may not use this file
+// except in compliance with the license. The license can be found in the root
+// directory of this project, or at
+// 
+// http://opensource.org/licenses/MIT
+
 #ifndef OSSIACO_CONVERTER_PRINT_TYPE_NAME_HPP
 #define OSSIACO_CONVERTER_PRINT_TYPE_NAME_HPP
 
