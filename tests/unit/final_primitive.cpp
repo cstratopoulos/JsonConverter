769de305aadec2e0b2ee23008b19b4a8607279a1
Christos Stratopoulos <c.stratopoulos@gmail.com>
2018-04-18 16:43:54 -0400

    Create dedicated folder for test types

--- tests/unit/final_primitive.cpp
+++ tests/unit/final_primitive.cpp
@@ -12,6 +12,7 @@
 // only primitive-convertible members
 
 #include "test_utils.hpp"
+#include "types/point_3d.hpp"
 
 #include <ossiaco/converter/adapt.hpp>
 #include <ossiaco/converter/allocate/simple.hpp>
@@ -20,38 +21,18 @@
 
 #include <tuple>
 
-struct Point3D {
-    OSSIACO_CONVERTER_FINAL_SUPPORTED(
-        Point3D,
-        (&Point3D::_x, OSSIACO_XPLATSTR("x"))
-        (&Point3D::_y, OSSIACO_XPLATSTR("y"))
-        (&Point3D::_z, OSSIACO_XPLATSTR("z")));
-
-    double _x{0.0};
-    double _y{0.0};
-    double _z{0.0};
-};
-
-bool operator==(const Point3D& p1, const Point3D& p2)
-{
-    return std::tie(p1._x, p1._y, p1._z) == std::tie(p2._x, p2._y, p2._z);
-}
-
-bool operator!=(const Point3D& p1, const Point3D& p2)
-{
-    return !(p1 == p2);
-}
+namespace tt = test_types;
 
 TEST_CASE("Converting 3D points", "[final]") 
 {
     runTestCases(
-        makeSimpleStringTest("The origin", [] { return Point3D{}; }),
+        makeSimpleStringTest("The origin", [] { return tt::Point3D{}; }),
         makeSimpleStringTest(
             "A unit vector",
             [] {
-                return Point3D{0.0, 1.0, 0.0};
+                return tt::Point3D{0.0, 1.0, 0.0};
             }),
         makeSimpleStringTest("Some arbitrary vector", [] {
-            return Point3D{-1.5, 1000.12345, -99.87654321};
+            return tt::Point3D{-1.5, 1000.12345, -99.87654321};
         }));
 }
