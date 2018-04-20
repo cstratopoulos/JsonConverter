cce69242edc34787e33286431ec30b7bd7d133ff
Christos Stratopoulos <c.stratopoulos@gmail.com>
2018-04-20 14:08:24 -0400

    Add tests of unregistered type and abstract allocation

--- tests/unit/poly_decorator/abstract_base.cpp
+++ tests/unit/poly_decorator/abstract_base.cpp
@@ -25,6 +25,17 @@ auto makeShapeTest(std::string_view desc, Func&& func)
         StringObjectConversion{});
 }
 
+struct OopsShape : tt::Shape {
+    ~OopsShape() override {}
+
+    tt::Point3D center() const override { return {}; }
+
+    OSSIACO_CONVERTER_POLY_SUPPORTED(
+        OopsShape, test_types::Shape, (&OopsShape::_someInt, OSSIACO_XPLATSTR("someInt")));
+
+    int _someInt{};
+};
+
 TEST_CASE("Converting inheritors of a pure interface ABC", "[PolyDecoratorAllocator]")
 {
     runTestCases(
@@ -33,4 +44,57 @@ TEST_CASE("Converting inheritors of a pure interface ABC", "[PolyDecoratorAlloca
         makeShapeTest("A line segment", [] {
             return tt::Segment(tt::Point3D{0.0, 26.0, -13.0}, tt::Point3D{1.0, 939293.4, 332.2353});
         }));
+}
+
+TEST_CASE("Type decorator allocation errors", "[PolyDecoratorAllocator]")
+{
+    namespace Oc = Ossiaco::converter;
+
+    SECTION("Trying to allocate an abstract type")
+    {
+        const auto jsonString = OSSIACO_XPLATSTR(
+            R"--(
+{
+"@type": "test_types::Shape"
+}
+)--");
+        CAPTURE(jsonString);
+        CHECK_THROWS_AS(
+            Oc::JsonDeserializer<tt::Shape>::fromString(jsonString),
+            Oc::AbstractTypeAllocation<tt::Shape>);
+    }
+
+    SECTION("Unregistered type errors")
+    {
+        SECTION("Trying to deserialize from a class which is not derived from base (and does not exist!)")
+        {
+            const auto jsonString = OSSIACO_XPLATSTR(
+                R"--(
+{
+"@type": "some_namespace::SomeClass",
+"field": "value"
+}
+)--");
+            CAPTURE(jsonString);
+
+            CHECK_THROWS_AS(
+                Oc::JsonDeserializer<tt::Shape>::fromString(jsonString),
+                Oc::UnregisteredType<tt::Shape>);
+        }
+
+        SECTION("Trying to deserialize from a genuine inheritor which has not been registered")
+        {
+            OopsShape oops{};
+            const auto jsonString = Oc::toJsonStringPretty(oops);
+            CAPTURE(jsonString);
+
+            CHECK_THROWS_AS(
+                Oc::JsonDeserializer<tt::Shape>::fromString(jsonString),
+                Oc::UnregisteredType<tt::Shape>);
+
+            CHECK(Oc::jsonPolyImpl<OopsShape>());
+
+            CHECK_NOTHROW(Oc::JsonDeserializer<tt::Shape>::fromString(jsonString));
+        }
+    }
 }
