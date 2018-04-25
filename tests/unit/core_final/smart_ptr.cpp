30cdf306b840856bf9b0863c9ac876ad65fc0bea
Christos Stratopoulos <c.stratopoulos@gmail.com>
2018-04-25 17:46:14 -0400

    Add more digraph tests

--- tests/unit/core_final/smart_ptr.cpp
+++ tests/unit/core_final/smart_ptr.cpp
@@ -13,12 +13,39 @@
 
 namespace tt = test_types;
 
-TEST_CASE("Directed graph with containers of smart pointers", "[shared_ptr][weak_ptr][ConvertSmartPtr]")
+TEST_CASE(
+    "Directed graph with containers of smart pointers", "[shared_ptr][weak_ptr][ConvertSmartPtr]")
 {
-    runTestCases(makeSimpleStringTest("A path on three vertices", [] {
-        return tt::Digraph({
-            {OSSIACO_XPLATSTR("a"), OSSIACO_XPLATSTR("b")},
-            {OSSIACO_XPLATSTR("b"), OSSIACO_XPLATSTR("c")},
-        });
-    }));
+    runTestCases(
+        makeSimpleStringTest(
+            "A path on three vertices",
+            [] {
+                return tt::Digraph({
+                    {OSSIACO_XPLATSTR("a"), OSSIACO_XPLATSTR("b")},
+                    {OSSIACO_XPLATSTR("b"), OSSIACO_XPLATSTR("c")},
+                });
+            }),
+        makeSimpleStringTest(
+            "A bidirectional edge",
+            [] {
+                return tt::Digraph({
+                    {OSSIACO_XPLATSTR("here"), OSSIACO_XPLATSTR("there")},
+                    {OSSIACO_XPLATSTR("there"), OSSIACO_XPLATSTR("here")},
+                });
+            }),
+        makeSimpleStringTest("The complete directed graph on 4 vxs", [] {
+            std::vector<Ossiaco::converter::string_t> vxs{OSSIACO_XPLATSTR("1"),
+                                                          OSSIACO_XPLATSTR("2"),
+                                                          OSSIACO_XPLATSTR("3"),
+                                                          OSSIACO_XPLATSTR("4")};
+
+            tt::Digraph g;
+
+            for (const auto& source : vxs)
+                for (const auto& sink : vxs)
+                    if (source != sink)
+                        g.addEdge(std::make_pair(source, sink));
+
+            return g;
+        }));
 }
