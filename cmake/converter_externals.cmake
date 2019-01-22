182dc5e78c60a8f48dd3095197b6d486550a5cca
Christos Stratopoulos <christos.stratopoulos@ossiaco.com>
2019-01-22 21:47:54 +0000

    Merged PR 312: Guard call to find_package(Catch2)
    While using this code as a submodule from another repo, I realized the call to `find_package` for Catch2 should only take place if tests are being run.

--- cmake/converter_externals.cmake
+++ cmake/converter_externals.cmake
@@ -11,11 +11,13 @@
 find_path(DATE_INCLUDE_DIRS date/date.h)
 find_path(RAPIDJSON_INCLUDE_DIRS rapidjson/rapidjson.h)
 
-find_package(Catch2 2.4.2 REQUIRED)
 find_package(range-v3 0.4.0 REQUIRED)
 
-if(${OSSIACO_CONVERTER_TESTS} AND ${OSSIACO_CONVERTER_BOOST_FS})
-    find_package(Boost 1.67.0 COMPONENTS filesystem REQUIRED)
-else()
-    find_package(Boost 1.67.0 REQUIRED)
+if(${OSSIACO_CONVERTER_TESTS})
+    find_package(Catch2 2.4.2 REQUIRED)
+    if(${OSSIACO_CONVERTER_BOOST_FS})
+        find_package(Boost 1.67.0 COMPONENTS filesystem REQUIRED)
+    else()
+        find_package(Boost 1.67.0 REQUIRED)
+    endif()
 endif()
